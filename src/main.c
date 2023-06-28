9537ade1ae06b0d008919381209d6748c3f8dd84
nginr <zarifhossainafif@gmail.com>
2023-06-29 00:05:35 +0600

    Added window centering functionality.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <raylib.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -17,6 +18,8 @@ const int cellBlock = 25;
 const int cellCount = 25;
 
 bool event_triggered(double duration);
+void center_window(size_t win_w, size_t win_h);
+void exit_menu(size_t windowWall);
 
 int main(void) {
     srand(getpid() + time(NULL));
@@ -25,6 +28,7 @@ int main(void) {
 
     InitWindow(windowWall, windowWall, windowName);
     SetTargetFPS(60);
+    center_window(windowWall, windowWall);
 
     Snake s = {0};
     Food  fs[Count_FoodType];
@@ -67,4 +71,19 @@ bool event_triggered(double duration) {
         return true;
     }
     return false;
+}
+
+void center_window(size_t win_w, size_t win_h) {
+    int monitor = GetCurrentMonitor();
+
+    int m_w = GetMonitorWidth(monitor);
+    int m_h = GetMonitorHeight(monitor);
+    SetWindowPosition(m_w * 0.5 - win_w * 0.5, m_h * 0.5 - win_h * 0.5);
+}
+
+void exit_menu(size_t windowWall) {
+    char exit_buffer[256] = "Are you sure you want to exit program? [Y/N]";
+    DrawRectangle(0, windowWall * 0.5 - 2.5 * cellBlock, windowWall,
+                  5 * cellBlock, BLACK);
+    DrawText(exit_buffer, windowWall * 0.1, windowWall * 0.5, 20, WHITE);
 }
